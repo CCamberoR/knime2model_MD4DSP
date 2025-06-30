9d75473a8427606bbd056bd23107aca6aca31cb6
CARLOS BREUER CARRASCO <cbc@unex.es>
2025-07-01 00:14:21 +0200

    removed unused function from library_functions.py

--- utils/library_functions.py
+++ utils/library_functions.py
@@ -3,30 +3,6 @@ import json
 from utils.logger import print_and_log
 
 
-def get_library_transformation_id(json_file_path: str, node_name: str) -> int | None:
-    """
-    Reads the JSON file and finds the hashing function whose identifier matches the value of the node_name variable.
-    Extracts the value of the library_transformation_id attribute from the first matching element.
-
-    Args:
-        json_file_path (str): Path to the JSON file.
-        node_name (str): Name of the node to search for.
-
-    Returns:
-        int: Value of the library_transformation_id attribute of the first matching element.
-    """
-    with open(json_file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-
-    functions_hashing = data.get("functions_hashing", [])
-
-    for function in functions_hashing:
-        if node_name in function:
-            return function[node_name].get("library_transformation_id")
-
-    return None
-
-
 def get_library_transformation_names(json_file_path: str) -> list:
     """
     Reads the JSON file and returns a list with the library transformation names.
